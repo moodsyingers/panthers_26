c0cf5742ee7f7fbabc9b5402f5a921990838cebd
Priyanshu Bharadwaj <76169829+thelostdeveloper@users.noreply.github.com>
2022-06-26 11:14:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ document.querySelector('#cart-icon').onclick = () =>{
     cart.classList.toggle('active');
     search.classList.remove('active');
     user.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 let user = document.querySelector('.user');
@@ -20,4 +21,50 @@ document.querySelector('#user-icon').onclick = () =>{
     user.classList.toggle('active');
     search.classList.remove('active');
     cart.classList.remove('active');
+    navbar.classList.remove('active');
 }
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () =>{
+    navbar.classList.toggle('active');
+    search.classList.remove('active'); 
+     cart.classList.remove('remove');
+     user.classList.remove('active');
+} 
+window.onscroll = () => {
+   search.classList.remove('active'); 
+     cart.classList.remove('remove');
+     user.classList.remove('active');
+     navbar.classList.remove('active');
+}
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow' , window.scrollY > 0);
+});
+
+var swiper = new Swiper(".new-arrival",{
+    spaceBetween: 20,
+    loop: true,
+    autoplay:{
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints:{ 
+        0: {
+            slidesPerView: 0,
+        },
+        568: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
+    },
+});
